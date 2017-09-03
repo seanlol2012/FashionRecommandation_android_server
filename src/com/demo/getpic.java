f3d114e99ae6e198ae370023976e32b987158911
seanlol2012 <seanlol2012@github.com>
2017-09-03 19:36:43 +0800

    9.3:19.36

---
+++ src/com/demo/getpic.java
@@ -0,0 +1,74 @@
+package com.demo;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class getpic
+ */
+@WebServlet("/getpic")
+public class getpic extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public getpic() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String pic_name = "/home/gzx/UploadImg/";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+		Date date = new Date();
+		pic_name = pic_name + sdf.format(date);
+		pic_name = pic_name + ".jpg";
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("successful received");
+		
+		try(ServletInputStream sis = request.getInputStream()) {
+			OutputStream os = new FileOutputStream(pic_name);
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+			
+			byte[] buf = new byte[1024];
+			int length = 0;
+			
+			length = sis.readLine(buf, 0, buf.length);
+			while(length != -1) {
+				bos.write(buf, 0, length);
+				length = sis.read(buf);
+			}
+			sis.close();
+			bos.close();
+			os.close();
+		}
+	}
+
+}
